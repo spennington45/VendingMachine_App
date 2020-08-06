673ccfe50589c556b6232e6dd7355e6fe6ca0d8b
Steven Pennington <spennington45@gmail.com>
2020-08-06 10:52:28 -0400

    Push 1

--- Capstone/java/src/main/java/com/techelevator/VendingMachineCLI.java
+++ Capstone/java/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,34 +1,80 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import com.techelevator.view.Menu;
 
 public class VendingMachineCLI {
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
+	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
 
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT };
+
+	private static final String [] PURCHESE_MENU = {"Feed Money", "Select Product", "Back"};
+	private static final String [] MONEY_MENU = {"$1.00", "$2.00", "$5.00", "$10.00", "Back"};
+//	private static final List <String> SELECT_PRODUCT = readProducts();
 	private Menu menu;
 
 	public VendingMachineCLI(Menu menu) {
 		this.menu = menu;
 	}
 
-	public void run() {
+	public void run() throws FileNotFoundException {
 		while (true) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-				// display vending machine items
+				readProducts();
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-				// do purchase
+				processPurchaseMenuOption();
+			} else {
+				System.exit(0);
 			}
 		}
 	}
 
-	public static void main(String[] args) {
+	private void processPurchaseMenuOption() {
+		String purchaseMenuOption = "";
+		while (!purchaseMenuOption.equals("Back")) {
+			purchaseMenuOption = (String) menu.getChoiceFromOptions(PURCHESE_MENU);
+			if (purchaseMenuOption.equals("Feed Money")) {
+				processMoneyFeed();
+			} 
+		}
+	}
+
+	private void processMoneyFeed() {
+		String feedOptions = "";
+		while (!feedOptions.equals("Back")) {
+			feedOptions = (String) menu.getChoiceFromOptions(MONEY_MENU);					
+		}
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
 		Menu menu = new Menu(System.in, System.out);
 		VendingMachineCLI cli = new VendingMachineCLI(menu);
 		cli.run();
+	}
+	
+	public static void readProducts() throws FileNotFoundException {
+		File products = new File("vendingmachine.csv");
+		List <String> readList = new ArrayList <String> ();
+		if (products.exists()) {
+			try (Scanner fileScanner = new Scanner(products)) {
+				while (fileScanner.hasNextLine()) {
+				String items = fileScanner.nextLine();
+				readList.add(items);
+				}
+			} 
+		}
+		for (String i : readList) {
+			System.out.println(i);
+		}
 	}
 }
