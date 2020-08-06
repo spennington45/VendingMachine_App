b3395667bd7fadab8cece01e81bf689752014a49
Steven Pennington <spennington45@gmail.com>
2020-08-06 12:02:14 -0400

    Chips and Candy classes filled out

--- Capstone/java/src/main/java/com/techelevator/Candy.java
+++ Capstone/java/src/main/java/com/techelevator/Candy.java
@@ -8,30 +8,35 @@ public class Candy implements MasterItemType {
 
 	private Map <String, BigDecimal> item = new HashMap <String, BigDecimal> ();
 	private String slot;
-	private int quantity;
+	private int quantity = 5;
+	private String name;
+
 	
+
+	public Candy(String slot, String name, BigDecimal price) {
+		this.slot = slot;
+		this.item.put(name,price);
+		this.name = name;
+	}
+
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
 	@Override
 	public String getSlot() {
-		// TODO Auto-generated method stub
-		return null;
+		return slot;
 	}
 
 	@Override
 	public int getQuantity() {
-		// TODO Auto-generated method stub
-		return 0;
+		return quantity;
 	}
 
 	@Override
 	public BigDecimal getPrice() {
-		// TODO Auto-generated method stub
-		return null;
+		return item.get(name);
 	}
 
 }
